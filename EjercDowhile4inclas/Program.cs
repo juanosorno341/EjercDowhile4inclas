544e111cf59e3230f07695d9c98358a38c71d975
juan carlos osorno <juan.osorno341@pascualbravo.edu.co>
2024-03-10 04:15:54 -0500

    Agregar archivos de proyecto.

---
+++ EjercDowhile4inclas/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercDowhile4inclas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerodecuenta = 0;
+            float saldoactual = 0;
+            float suma = 0;
+
+            do
+            {
+
+                Console.WriteLine(" ingrese numero de cuenta corriente ");
+                numerodecuenta = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(" ingrese saldo actual ");
+                saldoactual = float.Parse(Console.ReadLine());
+
+
+
+                if (saldoactual > 0)
+                {
+                    suma += saldoactual;
+                    Console.WriteLine($" estado de la cuenta: Activo. acrededor con saldo de cuenta : $ " +  saldoactual);
+                    
+                    
+                }
+                else if (saldoactual < 0)
+
+                {
+                    Console.WriteLine(" cliente deudor y su mora acutual es de : $ " + saldoactual + " ;  para mas informacion comuniquese con la linea 01800000000.  ");
+                }
+                else
+
+                {
+                    Console.WriteLine(" saldo nulo ");
+                }
+            }
+
+            while (numerodecuenta > 0);
+            {
+                Console.WriteLine(" Total suma saldos acrededores. $" + suma);
+                
+
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
